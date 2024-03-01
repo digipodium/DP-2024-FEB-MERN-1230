c3661a12325626a42501bdd942f22dd9268fad9a
m-triple-m <triplem656@gmail.com>
2024-03-01 14:00:03 +0530

    link

--- Section 2/frontend/src/app/login/page.jsx
+++ Section 2/frontend/src/app/login/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import classes from './login.module.css';
 
 const Login = () => {
   return (
     <div>
         <h3>My Login Page</h3>
+        <button className={classes.loginBtn}>Login Submit</button>
+        <button className='btn btn-primary'>My Button</button>
     </div>
   )
 }
 
 export default Login;
+
+// signup
+// contact
+// about
--- Section 2/frontend/src/app/page.jsx
+++ Section 2/frontend/src/app/page.jsx
@@ -1,10 +1,20 @@
+import Link from 'next/link';
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <h1>My Home Page</h1>
-      <button>Nice</button>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Signup</Link>
+      <Link href="/contact">Contact</Link>
+      <Link href="/about">About</Link>
+      <h1 style={{ color : 'red', fontSize : '3em' }} >My Home Page</h1>
+
+      <hr />
+      <br />
+      <input type="text" />
+
+      <button className='my-btn'>Nice</button>
     </div>
   )
 }
